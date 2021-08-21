6004e6afe719dafa34845816798a9ce7e012c46f
adilask127 <adilask127@gmail.com>
2021-08-21 15:44:54 +0600

    clicksmodule.js , app.js files modified
    Написал класс по подсчету кликов.
    И указал его вызов с параметрами

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import './styles.css'
+import {ClicksModule} from './modules/clicks.module'
 
+
+
+const initiateClicksCalcs = new ClicksModule (Number, "30000");
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,53 @@
 import {Module} from '../core/module'
 
 export class ClicksModule extends Module {
+    #numberOfClicks
+    #numberOfDblClicks
 
+    constructor(type, text) {
+        super(type, text);
+        console.log('invoke character');
+        this.#numberOfClicks = 0;
+        this.#numberOfDblClicks = 0;
+        const elementHTMLClick =this.#selectHTMLElement('html');
+        const elementHTMLDoubleClick =this.#selectHTMLElement('body') 
+        this.#calculateNumberOfClicks(elementHTMLClick);
+        this.#calculateNumberOfDoubleClicks(elementHTMLDoubleClick);
+        setTimeout(() => { 
+            alert(
+                `Click count: ${this.#NumberOfSingleClicks()}, DoubleClick count: ${this.#NumberOfDoubleClicks()}`
+            );}, type(text));
+    }
+
+    #NumberOfSingleClicks() {
+        return this.#numberOfClicks;
+    }
+
+    #NumberOfDoubleClicks() {
+        return this.#numberOfDblClicks;
+    }
+
+    #selectHTMLElement(elementDOM) {
+        const elementHTML = document.querySelector(elementDOM);
+        return elementHTML;
+    }
+
+    #calculateNumberOfClicks(elementHTML) {
+        elementHTML.addEventListener('click', event => {
+                this.#numberOfClicks += 1;
+                console.log(`Click count: ${this.#numberOfClicks}`);
+
+        });
+    }
+
+    #calculateNumberOfDoubleClicks(elementHTML) {
+        elementHTML.addEventListener('dblclick', event => {
+            event.stopPropagation ? event.stopPropagation() : (event.cancelBubble=true);
+            this.#numberOfDblClicks += 1;
+            this.#numberOfClicks = this.#numberOfClicks >= 2 
+                ? this.#numberOfClicks - 2 
+                : this.#numberOfClicks;
+            console.log(`Double Click count: ${this.#numberOfDblClicks}`);
+        });
+    }
 }
