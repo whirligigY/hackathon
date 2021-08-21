a0b23b08716b69ad8c10d6ef00e7ae4300de71ca
Yulia <yulia.v@Yulias-MacBook.local>
2021-08-21 14:19:09 +0700

    Create menu module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-import './styles.css'
+import "./styles.css";
+import { ContextMenu } from "./menu";
 
+document.body.addEventListener("contextmenu", (ev) => {
+  ev.preventDefault();
+  const contextmenu = new ContextMenu("#menu");
+  contextmenu.open(ev);
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,40 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+    this.content = "";
+    this.module = {};
+    this.type = "";
+  }
+  createElement(Module, type, text) {
+    const module = new Module(type, text);
+    this.module = { ...this.module, [type]: module };
+    this.content += module.toHTML();
+    this.el.innerHTML = this.content;
+    this.type = "";
+  }
+  open(ev) {
+    this.el.style.top = `${ev.clientY}px`;
+    this.el.style.left = `${ev.clientX}px`;
+    this.el.classList.add("open");
+    this.createElement(BackgroundModule, "font", "Поменять цвет");
+    this.createElement(ClicksModule, "clicks", "Считать клики (за 3 секунды)");
+    this.el.addEventListener("click", (event) => {
+      const { target } = event;
+      if (target.nodeName === "LI") {
+        this.type = target.getAttribute("data-type");
+      }
+      this.module[this.type] && this.module[this.type].trigger();
+      this.close();
+    });
+  }
 
+  close() {
+    this.el.classList.remove("open");
+    this.content = "";
+    this.module = {};
+  }
 }
